df44260eb6830e42c8c8ad93acf455e9c2cccdba
NagarajGouda07 <hnagaraj.xworkz@gmail.com>
2024-07-19 10:22:06 +0530

    inital

---
+++ arrays/Characters.java
@@ -0,0 +1,30 @@
+class Characters {
+
+ public static void main(String[] args)
+ {
+  char name='V';
+  char name1='i';
+  char name2='r';
+  char name3='e';
+  char name4='s';
+  char name5='h';
+  char name6='M';
+  char name7='R';
+  char name8='u';
+  char name9='d';
+  char name10='r';
+  char name11='a';
+  char name12='g';
+  char name13='o';
+  char name14='u';
+  char name15='d';
+  char name16='a';
+  char name17='r';
+  char name18='V';
+  char name19='R';
+  
+  char[] fullname={name,name1,name2,name3,name4,name5,name6,name7,name8,name9,name10,name11,name12,name13,name14,name15,name16,name17,name18,name19};
+  int names=fullname.length;
+   System.out.println("Number of characters "+names);
+ }
+}
