03d8bfe328097effc5860c893b19c2726c43750a
NagarajGouda07 <hnagaraj.xworkz@gmail.com>
2024-08-08 06:34:12 +0530

    add files

---
+++ associate/Country.java
@@ -0,0 +1,19 @@
+class Country 
+{
+    String name;
+    String continent;
+    State state;
+
+    Country(String name, String continent, State state) 
+	{
+        this.name = name;
+        this.continent = continent;
+        this.state = state;
+    }
+
+    void printDetails() 
+	{
+        System.out.println("Country Name: " + name + ", Continent: " + continent);
+        state.printDetails();
+    }
+}
