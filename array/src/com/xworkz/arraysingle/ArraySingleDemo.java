a1cd74236a83911cff46fc5d4273d1342704e0b5
NagarajGouda07 <hnagaraj.xworkz@gmail.com>
2024-09-19 22:22:06 +0530

    array one & multidimension

---
+++ array/src/com/xworkz/arraysingle/ArraySingleDemo.java
@@ -0,0 +1,27 @@
+package com.xworkz.arraysingle;
+
+public class ArraySingleDemo {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5, };
+
+		//System.out.println(arr[0]);
+		//System.out.println(arr[1]);
+		//System.out.println(arr[2]);
+		//System.out.println(arr[3]);
+		//System.out.println(arr[4]);
+
+		// modify
+		// arr[4] = 7;
+		// System.out.println(arr[4]);
+
+		//for (int i = 0; i < arr.length; i++) {
+			//System.out.println(arr[i]);
+		//}
+		for(int y:arr) {
+			System.out.println(y);
+		}
+	}
+
+}
---
+++ array/src/com/xworkz/arraysingle/ArrayString.java
@@ -0,0 +1,22 @@
+package com.xworkz.arraysingle;
+
+public class ArrayString {
+
+	public static void main(String[] args) {
+
+		String arr[] = { "naga", "raja", "rajesh", "gouda","mamu","rocky" };
+
+		//for loop
+		//for(int i=0; i<arr.length; i++) {
+			//System.out.println(arr[i]);
+		//}
+		
+		//for each
+		for (String ref:arr) {
+			System.out.println(ref);
+			
+		}
+
+	}
+
+}
